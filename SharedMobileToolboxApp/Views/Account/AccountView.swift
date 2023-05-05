1876c96730d18da38e699de9212b689fc1aad15a
Shannon Young <shannon.young@sagebase.org>
2023-05-05 12:20:12 -0700

    Hide the logout button

--- SharedMobileToolboxApp/Views/Account/AccountView.swift
+++ SharedMobileToolboxApp/Views/Account/AccountView.swift
@@ -66,10 +66,12 @@ struct AccountView: View {
         VStack(spacing: 0) {
             accountInfoView()
             withdrawalView()
+            #if DEBUG
             Button(action: { bridgeManager.signOut() }) {
                 Text("Log out")
             }
             .buttonStyle(NavigationButtonStyle(.text))
+            #endif
         }
     }
     
