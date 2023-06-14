a635fe7534728818f070a96d2279545d6ebd8825
Shannon Young <shannon.young@sagebase.org>
2023-06-14 11:41:49 -0700

    Change to using an AppDelegate and a SceneDelegate
    This allows the app to honor all rotations but still lock to portrait or landscape when needed.

--- SharedMobileToolboxApp/MobileTooboxAppDelegate.swift
+++ SharedMobileToolboxApp/MobileTooboxAppDelegate.swift
@@ -17,7 +17,7 @@ import MobileToolboxKit
 //import MotionSensor
 //import AudioRecorder
 
-open class MobileToolboxAppDelegate: RSDSwiftUIAppDelegate, ReauthPasswordHandler {
+open class MobileToolboxAppDelegate: RSDAppDelegate, ReauthPasswordHandler {
     open class var appId: String { "" }
     open class var pemPath: String { "" }
     
@@ -29,6 +29,15 @@ open class MobileToolboxAppDelegate: RSDSwiftUIAppDelegate, ReauthPasswordHandle
         self.bridgeManager = SingleStudyAppManager(appId: appId, pemPath: pemPath)
         super.init()
         self.bridgeManager.reauthPasswordHandler = self
+    }
+    
+    open func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    open override func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        AppOrientationLockUtility.shouldAutorotate = true
+        return super.application(application, willFinishLaunchingWithOptions: launchOptions)
     }
     
     open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
